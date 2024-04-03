bba26f77e15082d0047caf6aa5de533d5b6c5cc3
KirtiSingh <kirtisingh1497@gmail.com>
2024-04-04 02:20:29 +0530

    learned dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all code as newer version
+
+//alert("hello") // we are using node js not browser
+
+//console.log(3+3) console.log("Kirti") this code is not readable
+
+let name = "Kirti"
+let age = 24
+let logedIn = false
+
+// Primitive Data type
+// number => 2 to power 53
+//bigInt
+//string =>""
+//boolean => true/false
+//null => standalone value / empty
+//undefined => when value is not defined
+//symbol => for uniqueness
+
+
+//Object
+
+console.log(typeof(3))
+console.log(typeof(name))
+console.log(typeof(null))
+console.log(typeof(undefined))
