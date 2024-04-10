57b6f58bd5b4bb616a6cd9f7ae05fd8368fbafb6
KirtiSingh <kirtisingh1497@gmail.com>
2024-04-10 14:24:37 +0530

    notes on array

---
+++ array.js
@@ -0,0 +1,4 @@
+//array is an object used for storing multiple data under a single variable name and array have method to perform operation on it
+
+// array are resizable , follows shallow copy , can be access by non negative integer
+
