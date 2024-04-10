55871fc44b8957882c739d7fc5188297bb59f97a
KirtiSingh <kirtisingh1497@gmail.com>
2024-04-10 19:11:46 +0530

    added comment for accessing the object properties

--- object.js
+++ object.js
@@ -10,8 +10,9 @@ office:"Mindfire",
 "full name": "Kirti Singh"
 }
 
-console.log(user.name)
+console.log(user.name)//Dot notation is commonly used when the property name is a valid JavaScript identifier
 console.log(user["full name"])
+//Bracket notation is necessary when the property name: contains special characters or starts with a digit,Is stored in a variable,Is dynamically generated.
 
 user.name = "Kittu"
 console.log(user)
