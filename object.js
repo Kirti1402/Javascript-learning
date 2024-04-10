66f5657cc675de1bcc2a76fdbe9720006950a4a3
KirtiSingh <kirtisingh1497@gmail.com>
2024-04-10 19:09:42 +0530

    learn about symbol and object freeze

--- object.js
+++ object.js
@@ -3,7 +3,7 @@
 //object literals
 const mySym = Symbol("test");
 const user = {
-[mySym]:"test123",
+[mySym]:"test123", // if you dont use [mysym] type of will string 
 name:"kirti",
 office:"Mindfire",
 0:"xyz",
@@ -12,3 +12,10 @@ office:"Mindfire",
 
 console.log(user.name)
 console.log(user["full name"])
+
+user.name = "Kittu"
+console.log(user)
+
+Object.freeze(user)// to freeze he Object
+user.name = "Kittuda"
+console.log(user)
