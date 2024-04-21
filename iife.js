a09181d88ef970553454be97ee4da93351dc7cdf
KirtiSingh <kirtisingh1497@gmail.com>
2024-04-21 21:30:51 +0530

    Comments : updated full form

--- iife.js
+++ iife.js
@@ -6,7 +6,7 @@ function chai() {
 
 chai(); // function expression
 /* finding (why I added a semicolon after function expression
-ASI and Function Calls: JavaScript's ASI feature automatically inserts semicolons at the end of lines if they are missing. 
+ASI and Function Calls: JavaScript's ASI (Automatic Semicolon Insertion) feature automatically inserts semicolons at the end of lines if they are missing. 
 However, ASI doesn't always work as expected, especially when it comes to function expressions followed by function calls on separate lines. 
 In such cases, ASI may not insert semicolons where you expect, leading to errors.)
     */
